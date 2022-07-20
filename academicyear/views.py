4aa2ba5c8a3f776d8eaadaf87fcc35dac60c5c0c
Elyas Hedayat <98589320+elyashedayat10@users.noreply.github.com>
2022-07-20 18:56:22 +0430

    Update views.py

--- academicyear/views.py
+++ academicyear/views.py
@@ -4,7 +4,7 @@ from .forms import AcademicYearForm
 from django.views.generic import CreateView, ListView, View
 from django.urls import reverse_lazy
 from institute.models import Institute
-
+from student.models import Installment
 
 # Create your views here.
 
@@ -31,5 +31,18 @@ class AcademicYearDeleteView(View):
 class InstituteAcademicYearView(View):
     def get(self, request, *args, **kwargs):
         institute_id = kwargs.get('id')
+        institute_name=get_object_or_404(Institute,id=institute_id)
         obj=AcademicYear.objects.filter(institute=institute_id)
-        return render(request, 'academicyear/institute.html', {'object': obj})
+        return render(request, 'academicyear/institute.html', {'object': obj,'institute':institute_name})
+from django.db.models import  Sum,Count
+
+class PaidBaseOnYearInstitute(View):
+    def get(self,request,institute_id,year):
+        institute_obj=Institute.objects.get(id=institute_id)
+        installment_pay=Installment.objects.filter(institute_id=institute_id,academic_year_id=year)
+        total_pay=installment_pay.aggregate(Sum('amount'))['amount__sum'] or 0
+        installment_count=installment_pay.aggregate(Count('id'))['id__count']
+        paid=installment_pay.filter(paid=True).aggregate(Sum('amount'))['amount__sum'] or 0
+        unpaid=installment_pay.filter(paid=False).aggregate(Sum('amount'))['amount__sum'] or 0
+        return render(request,'academicyear/pay_archive.html',{
+            'total':installment_pay,'paid':paid,'unpaid':unpaid,'total_pay':total_pay,'count':installment_count,"institute_obj":institute_obj})
