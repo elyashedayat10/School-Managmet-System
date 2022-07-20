5a293c4afac9c2d4124faca5efa45aeae0bfadef
Elyas Hedayat <98589320+elyashedayat10@users.noreply.github.com>
2022-07-20 18:58:49 +0430

    Update urls.py

--- academicyear/urls.py
+++ academicyear/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    AcademicYearCreateView, AcademicYearListView, AcademicYearDeleteView,InstituteAcademicYearView
+    AcademicYearCreateView, AcademicYearListView, AcademicYearDeleteView,InstituteAcademicYearView,PaidBaseOnYearInstitute
 )
 
 app_name = 'academic_year'
@@ -9,5 +9,6 @@ urlpatterns = [
     path('list/', AcademicYearListView.as_view(), name='list'),
     path('create/', AcademicYearCreateView.as_view(), name='create'),
     path('delete/<int:pk>/', AcademicYearDeleteView.as_view(), name='delete'),
-    path('institute/<int:id>/',InstituteAcademicYearView.as_view(),name='institute_academic')
+    path('institute/<int:id>/',InstituteAcademicYearView.as_view(),name='institute_academic'),
+    path('institute/<int:institute_id>/<int:year>/',PaidBaseOnYearInstitute.as_view(),name='paid_archive'),
 ]
